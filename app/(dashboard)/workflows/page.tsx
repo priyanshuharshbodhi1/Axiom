84d9a0c1eca3847911cf252b15b3dce4343e8735
priyanshuharshbodhi1 <priyanshuqpwp@gmail.com>
2025-08-20 19:17:28 +0530

    feat: widget workflows page

---
+++ app/(dashboard)/workflows/page.tsx
@@ -0,0 +1,24 @@
+
+import React, { Suspense } from "react";
+
+
+function page() {
+  return (
+    <div className="flex-1 flex flex-col h-full">
+      <div className="flex justify-between">
+        <div className="flex flex-col">
+          <h1 className="text-3xl font-bold">Workflows</h1>
+          <p className="text-muted-foreground">Manage your workflows</p>
+        </div>
+     
+      </div>
+
+      <div className="h-full py-6">
+        
+      </div>
+    </div>
+  );
+}
+
+
+export default page;
